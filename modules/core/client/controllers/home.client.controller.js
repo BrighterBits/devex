ae9e0579624494040a3eb08b83854906c875cf17
Andrew Sutherland <sutherlanda@gmail.com>
2018-07-11 09:54:55 -0700

    Refactor controllers, services to enable minifying

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,17 +1,21 @@
 (function () {
   'use strict';
 
+  var HomeController = [
+    'Authentication',
+    '$state',
+    function (Authentication, $state) {
+      var vm = this;
+      vm.isUser = Authentication.user;
+      if (sessionStorage.prevState) {
+        var prevState = sessionStorage.prevState;
+        delete sessionStorage.prevState;
+        $state.go(prevState);
+      }
+    }
+  ];
+
   angular
     .module('core')
     .controller('HomeController', HomeController);
-
-  function HomeController(Authentication, $state) {
-    var vm = this;
-    vm.isUser = Authentication.user;
-    if (sessionStorage.prevState) {
-      var prevState = sessionStorage.prevState;
-      delete sessionStorage.prevState;
-      $state.go(prevState);
-    }
-  }
 }());
